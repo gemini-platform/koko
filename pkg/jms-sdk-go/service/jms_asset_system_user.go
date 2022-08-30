5168025bbe82ffc001e9f64053a8cbfb69e2ff38
Xuanxin Zhen <xuanxin.zhen@gmail.com>
2022-08-30 16:01:49 +0800

    fix: 修改生成 system-user 逻辑

--- pkg/jms-sdk-go/service/jms_asset_system_user.go
+++ pkg/jms-sdk-go/service/jms_asset_system_user.go
@@ -6,12 +6,12 @@ import (
 	"github.com/jumpserver/koko/pkg/jms-sdk-go/model"
 )
 
-func (s *JMService) CreateAssetSystemUser(name, username, privateKey, protocol string) (user model.SystemUser, err error) {
-	params := map[string]string{
-		"name":        name,
-		"username":    username,
-		"private_key": privateKey,
-		"protocol":    protocol,
+func (s *JMService) CreateAssetSystemUser(name, username string) (user model.SystemUser, err error) {
+	params := map[string]interface{}{
+		"name":              name,
+		"username":          username,
+		"auto_generate_key": true,
+		"protocol":          model.ProtocolSSH,
 	}
 	_, err = s.authClient.Post("/api/v1/assets/system-users/", params, &user)
 	return
@@ -22,12 +22,13 @@ func (s *JMService) GetAssetSystemUser(id string) (user model.SystemUser, err er
 	return
 }
 
-func (s *JMService) UpdateAssetSystemUser(id, name, username, privateKey, protocol string) (user model.SystemUser, err error) {
+func (s *JMService) UpdateAssetSystemUser(id, name, username, privateKey, publicKey string) (user model.SystemUser, err error) {
 	params := map[string]string{
 		"name":        name,
 		"username":    username,
 		"private_key": privateKey,
-		"protocol":    protocol,
+		"public_key":  publicKey,
+		"protocol":    model.ProtocolSSH,
 	}
 	_, err = s.authClient.Put(fmt.Sprintf("/api/v1/assets/system-users/%s/", id), params, &user)
 	return
